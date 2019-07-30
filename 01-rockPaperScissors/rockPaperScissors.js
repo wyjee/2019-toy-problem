c51fa000b78b3b49051f32a82b07e1c254ae5293
eunji <international0523@gmail.com>
2019-07-30 10:00:40 +0900

    toy1

--- 01-rockPaperScissors/rockPaperScissors.js
+++ 01-rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,46 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
+var rockPaperScissors = function () {
+
+  if([...arguments][0]<1) return 'There\'s no play'
+  
+  if([...arguments].length===0) {
+        let result = [];
+        let choices = ['rock', 'scisscors', 'paper'];
+        for (let i = 0; i < choices.length; i++) {
+          for (let j = 0; j < choices.length; j++) {
+            for (let k = 0; k < choices.length; k++) {
+              let oneRound = [];
+              oneRound.push(choices[i], choices[j], choices[k]);
+              result.push(oneRound);
+            }
+          }
+        }
+        return result;
+  }
+
+  return function() {
+        let count = [...arguments][0];
+        let innerPlay = function() {
+                let result = [];
+                let choices = ['rock', 'scisscors', 'paper'];
+                for (let i = 0; i < choices.length; i++) {
+                        let oneRound = [];
+                        oneRound.push(choices[i], choices[j], choices[k]);
+                        result.push(oneRound);
+                        count--;
+                        if(count) {
+                                innerPlay()
+                        }
+                        else {
+                                return result;
+                        }
+                }
+        }
+
+  }
 };
+
+  //매개변수가 없는 경우   3판 new Array(3)
+  //let rock=3; let scissor=3; let paper=3
