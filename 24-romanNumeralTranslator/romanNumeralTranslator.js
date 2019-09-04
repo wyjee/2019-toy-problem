28857240258e3f285aa9fbb5c9b5cc0c36798ef7
leeeunji <ej.lee005@gmail.com>
2019-09-04 09:44:01 +0900

    toy problem 24 complete

--- 24-romanNumeralTranslator/romanNumeralTranslator.js
+++ 24-romanNumeralTranslator/romanNumeralTranslator.js
@@ -1,4 +1,3 @@
-
 /**
  * Given a roman numeral as input, write a function that converts the roman
  * numeral to a number and outputs it.
@@ -26,7 +25,27 @@ var DIGIT_VALUES = {
   M: 1000
 };
 
-var translateRomanNumeral = function(romanNumeral){
-// TODO: Implement me!
+var translateRomanNumeral = function(romanNumeral) {
+  // TODO: Implement me!
+  let result = 0;
+  let isSplit = romanNumeral.split("");
+  //하나씩 돌면서 DIGIT_VALUES 에 있는지 확인, 없으면 null
+  //있으면 다음 인덱스 숫자를 확인,
+  //다음 인덱스 숫자가 앞 숫자보다 작으면 결과값에 더하고
+  //다음 인덱스 숫자가 앞 숫자보다 크면 그 다음 숫자를 이 숫자에 뺀 다음에 더하기
 
+  for (let i = 0; i < isSplit.length; i++) {
+    console.log(isSplit[i], DIGIT_VALUES[isSplit[i]]);
+    if (DIGIT_VALUES[isSplit[i]] === undefined) return null;
+    else {
+      if (DIGIT_VALUES[isSplit[i]] < DIGIT_VALUES[isSplit[i + 1]]) {
+        result =
+          result + DIGIT_VALUES[isSplit[i + 1]] - DIGIT_VALUES[isSplit[i]];
+        i += 1;
+      } else {
+        result += DIGIT_VALUES[isSplit[i]];
+      }
+    }
+  }
+  return result;
 };
