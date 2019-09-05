77e97cd973d2948c6ce27fe9cfcd5372e539ef74
leeeunji <ej.lee005@gmail.com>
2019-09-05 09:46:10 +0900

    toy problem 25 hashTable submit without test

--- 25-hashTable/hashTable.js
+++ 25-hashTable/hashTable.js
@@ -1,25 +1,30 @@
+import { get } from "https";
+
 /**
  * Create a hash table with `insert()`, `retrieve()`, and `remove()` methods.
  * The hashtable does not need to resize but it should still handle collisions.
  */
 
-var makeHashTable = function(){
+var makeHashTable = function() {
   var result = {};
   var storage = [];
   var storageLimit = 1000;
-  result.insert = function(/*...*/ 
-){
+  result.insert = function(k, valueToHash /*...*/) {
     // TODO: implement `insert()`
+    let index = getIndexBelowMaxForKey(k, storageLimit);
+    storage.set(index, valueToHash);
   };
 
-  result.retrieve = function(/*...*/ 
-){
+  result.retrieve = function(k /*...*/) {
     // TODO: implement `retrieve()`
+    let index = getIndexBelowMaxForKey(k, storageLimit);
+    return get(index);
   };
 
-  result.remove = function(/*...*/ 
-){
+  result.remove = function(k /*...*/) {
     // TODO: implement `remove()`
+    let index = getIndexBelowMaxForKey(k, storageLimit);
+    delete storage.set(index);
   };
 
   return result;
@@ -28,10 +33,10 @@ var makeHashTable = function(){
 // This is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between
 // 0 and max - 1
-var getIndexBelowMaxForKey = function(str, max){
+var getIndexBelowMaxForKey = function(str, max) {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
-    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = (hash << 5) + hash + str.charCodeAt(i);
     hash = hash & hash; // Convert to 32bit integer
     hash = Math.abs(hash);
   }
