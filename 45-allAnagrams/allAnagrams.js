072db9c3aacecc0bf6e79b77ce3bb0b2f3ea6667
leeeunji <ej.lee005@gmail.com>
2019-10-02 09:55:00 +0900

    :zap: 4 passed, needs modifying

--- 45-allAnagrams/allAnagrams.js
+++ 45-allAnagrams/allAnagrams.js
@@ -7,11 +7,28 @@
  */
 
 /**
-  * example usage:
-  * var anagrams = allAnagrams('abc');
-  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+ * example usage:
+ * var anagrams = allAnagrams('abc');
+ * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
 
 var allAnagrams = function(string) {
   // Your code here.
+  // 키 값으로 각 글자값을 주자
+  // 남은 거는 붙여서 앞뒤로 슬라이스해서 쓰자
+
+  let result = {};
+  let copy = string.slice();
+  for (let i = 0; i < copy.length; i++) {
+    let spliced = copy.slice(0, i) + copy.slice(i + 1);
+    console.log(spliced);
+    for (let j = 1; j < spliced.length; j++) {
+      let new_spliced = spliced.slice(0, j) + spliced.slice(j + 1);
+      console.log("new", new_spliced);
+      result[string[i]] = string[i] + new_spliced;
+      console.log("object_result", result);
+    }
+  }
+  console.log("result", Object.values(result));
+  return Object.values(result);
 };
