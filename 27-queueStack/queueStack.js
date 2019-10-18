0b4b3c92eec944dfe7bcae2f379a64b9a032d076
leeeunji <ej.lee005@gmail.com>
2019-10-19 08:29:11 +0900

    toy-27

--- 27-queueStack/queueStack.js
+++ 27-queueStack/queueStack.js
@@ -1,47 +1,72 @@
 /**
- * Write a stack using your preferred instantiation pattern. 
+ * Write a stack using your preferred instantiation pattern.
  * Avoid using native array methods i.e., push, pop, and length.
  * Once you're done, implement a queue using two stacks.
  */
 
 /**
-  * Stack Class
-  */
+ * Stack Class
+ */
 var Stack = function() {
-
   // add an item to the top of the stack
-  this.push = function(){
+  this.storage = [];
+  this.size = 0;
+  this.push = function() {
+    this.storage[this.size] = value;
+    this.size++;
   };
 
   // remove an item from the top of the stack
-  this.pop = function(){
+  this.pop = function() {
+    this.storage.splice(this.size - 1, 1);
+    this.size--;
   };
 
   // return the number of items in the stack
-  this.size = function(){
+  this.size = function() {
+    return this.size;
   };
 };
 
 /**
-  * Queue Class
-  */
+ * Queue Class
+ */
 var Queue = function() {
   // Use two `stack` instances to implement your `queue` Class
-  var inbox = new Stack();
-  var outbox = new Stack();
+  this.inResult = new Stack();
+  this.deResult = new Stack();
 
   // called to add an item to the `queue`
-  this.enqueue = function(){
+  this.enqueue = function() {
     // TODO: implement `enqueue`
+    if (storage === "inbox") {
+      this.inResult.storage.push(value);
+      this.inResult.size++;
+    } else if (storage === "deResult") {
+      this.deResult.storage.push(value);
+      this.deResult.size++;
+    }
   };
 
   // called to remove an item from the `queue`
-  this.dequeue = function(){
+  this.dequeue = function() {
     // TODO: implement `dequeue`
+    if (storage === "inResult") {
+      this.inResult.storage.splice(0, 1);
+      this.inResult.size--;
+    } else if (storage === "deResult") {
+      this.deResult.storage.splice(0, 1);
+      this.deResult.size--;
+    }
   };
 
   // should return the number of items in the queue
-  this.size = function(){
+  this.size = function() {
     // TODO: implement `size`
+    if (storage === "inResult") {
+      return this.inResult.size;
+    } else if (storage === "deResult") {
+      return this.deResult.size;
+    }
   };
 };
