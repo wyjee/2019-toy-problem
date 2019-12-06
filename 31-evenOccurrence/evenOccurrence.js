5d9ab98241b7a9214da30cd6cb719694bd0c2d32
leeeunji <ej.lee005@gmail.com>
2019-12-06 17:44:39 +0900

    Complete toy-31

--- 31-evenOccurrence/evenOccurrence.js
+++ 31-evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one. 6
  * Return null if there are no even-occurrence items.
 */
 
@@ -12,4 +12,43 @@
 
 var evenOccurrence = function(arr) {
   // Your code here.
+    let acc = {};
+    for (let i=0; i<arr.length; i++) {
+      let current = arr[i]
+      if(acc[current]===undefined){
+        acc[current] = 1;
+      }
+      else if (typeof acc[current] === "string"){
+        continue;
+      }
+      
+      if(arr[i+1]){
+        let sliced = arr.slice(i + 1);
+        console.log('If it Exists',sliced.indexOf(current));
+        if(sliced.indexOf(current)>-1){
+
+      //if the curr number exists in Array 'sliced'
+        sliced.reduce((accumulator,curr)=>{
+          console.log('in reduce Method acc and curr',accumulator,curr)
+          if(current===curr){
+            acc[current] +=1;
+          }
+        },{})
+      //check total by reducing
+console.log('check total',acc)
+      //mark if 'odd' number
+      if(acc[current]%2===1){
+        acc[current]='odd'
+      }
+      //mark and return if even number
+      if(acc[current]%2===0){
+        acc[current]='even'
+        return current;
+      }
+      }
+      continue;
+      }
+      continue;
+    }
+    return null;
 };
