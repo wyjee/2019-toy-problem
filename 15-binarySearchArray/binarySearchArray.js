fc71780ca6b740285636cd23687217d4f7c3c6d6
leeeunji <ej.lee005@gmail.com>
2019-12-08 02:15:44 +0900

    Need more debugging

--- 15-binarySearchArray/binarySearchArray.js
+++ 15-binarySearchArray/binarySearchArray.js
@@ -8,6 +8,34 @@
  * console.log(index); // [3]
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (array, target, index) {
+    if(index === undefined) index = 0;
+    let middle = array.length ===1 ? 0 :Math.floor(array.length/2);
+    let midNum = array[middle];
+// console.log(
+//   "@",
+//   array,
+//   target,
+//   "Index:",
+//   index,
+//   "midNum === target",
+//   midNum,
+//   target,
+//   midNum === target
+// );
+    if(midNum === target) {
+        console.log(array,target,index + middle);
+        return index + middle;
+    }
+    if (target < midNum) {
+        let lowerSliced = array.slice(0, middle);
+        binarySearch(lowerSliced, target, index);
+    } else if (target > midNum) {
+        let highSliced = array.slice(middle);
+        binarySearch(highSliced, target, index + middle);
+    }
+    else {
+        return null;
+    }
 };
 
