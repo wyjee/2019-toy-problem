a139573601c2dd2709b289994b269a7118ea6e66
leeeunji <ej.lee005@gmail.com>
2019-12-15 15:38:44 +0900

    Second Complete deepEquelity

--- 09-deepEquality/deepEquality.js
+++ 09-deepEquality/deepEquality.js
@@ -1,25 +1,30 @@
 /**
-  * Write a function that, given two objects, returns whether or not the two
-  * are deeply equivalent--meaning the structure of the two objects is the
-  * same, and so is the structure of each of their corresponding descendants.
-  *
-  * Examples:
-  *
-  * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
-  * deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
-  *
-  * don't worry about handling cyclical object structures.
-  *
-  */
-  var deepEquals = function (apple, orange) {
-    
+ * Write a function that, given two objects, returns whether or not the two
+ * are deeply equivalent--meaning the structure of the two objects is the
+ * same, and so is the structure of each of their corresponding descendants.
+ *
+ * Examples:
+ *
+ * deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+ * deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+ *
+ * don't worry about handling cyclical object structures.
+ *
+ */
+var deepEquals = function(apple, orange) {
   if (Object.keys(apple).length !== Object.keys(orange).length) return false;
-    for (let key in apple) {
-      if (!orange.hasOwnProperty(key)) return false;
-        if (typeof (apple[key]) === "object") {
-          return deepEquals(apple[key], orange[key]);
-        }
-        if(apple[key] !== orange[key]) return false;
+
+  for (let key in apple) {
+    
+    if (typeof apple[key] === "object") {
+      // if (Array.isArray(apple[key])) {
+      //   for (let i = 0; i < apple[key].length; i++) {
+      //     return deepEquals(apple[key][i], orange[key][i]);
+      //   }
+      // }
+      return deepEquals(apple[key], orange[key]);
     }
-    return true;
+    if (apple[key] !== orange[key]) return false;
+  }
+  return true;
 };
