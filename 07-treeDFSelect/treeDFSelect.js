11bf9bff196c8e083dbaaa4ce4078e5540c87d41
leeeunji <ej.lee005@gmail.com>
2019-12-15 14:35:05 +0900

    Secod times TreeDFSelect Complete

--- 07-treeDFSelect/treeDFSelect.js
+++ 07-treeDFSelect/treeDFSelect.js
@@ -1,70 +1,70 @@
 /**
-  *
-  * Implement a `DFSelect` method on this Tree class.
-  *
-  * DFSelect accepts a filter function, calls that function on each of the nodes
-  * in Depth First order, and returns a flat array of node values of the tree
-  * for which the filter returns true.
-  *
-  * Example:
-  *   var root1 = new Tree(1);
-  *   var branch2 = root1.addChild(2);
-  *   var branch3 = root1.addChild(3);
-  *   var leaf4 = branch2.addChild(4);
-  *   var leaf5 = branch2.addChild(5);
-  *   var leaf6 = branch3.addChild(6);
-  *   var leaf7 = branch3.addChild(7);
-  *   root1.DFSelect(function (value, depth) {
-  *     return value % 2;
-  *   })
-  *   // [1, 5, 3, 7]
-  *
-  *   root1.DFSelect(function (value, depth) {
-  *     return depth === 1;
-  *   })
-  *   // [2, 3]
-  *
-  */
+ *
+ * Implement a `DFSelect` method on this Tree class.
+ *
+ * DFSelect accepts a filter function, calls that function on each of the nodes
+ * in Depth First order, and returns a flat array of node values of the tree
+ * for which the filter returns true.
+ *
+ * Example:
+ *   var root1 = new Tree(1);
+ *   var branch2 = root1.addChild(2);
+ *   var branch3 = root1.addChild(3);
+ *   var leaf4 = branch2.addChild(4);
+ *   var leaf5 = branch2.addChild(5);
+ *   var leaf6 = branch3.addChild(6);
+ *   var leaf7 = branch3.addChild(7);
+ *   root1.DFSelect(function (value, depth) {
+ *     return value % 2;
+ *   })
+ *   // [1, 5, 3, 7]
+ *
+ *   root1.DFSelect(function (value, depth) {
+ *     return depth === 1;
+ *   })
+ *   // [2, 3]
+ *
+ */
 
 /*
  * Basic tree that stores a value.
  */
 
-var Tree = function(value){
+var Tree = function(value) {
   this.value = value;
   this.children = [];
 };
 
 Tree.prototype.DFSelect = function(filter, depth = 0) {
   let result = [];
-  if(filter(this.value, depth)) {
-    result.push(this.value)}
+  if (filter(this.value, depth)) {
+    result.push((this.value));
+  }
+
   return [
     ...result,
-    ...(this.children.reduce((array,child)=>{
-      return array.concat(child.DFSelect(filter, depth+1))
-    },[]))
-  ]
+    ...(this.children.reduce((arr, child) => {
+      return arr.concat(child.DFSelect(filter,depth+1));
+    }, []))
+  ];
 };
-
-
 
 /**
  * You shouldn't need to change anything below here, but feel free to look.
-  */
+ */
 
 /**
-  * add an immediate child
-  * (wrap values in Tree nodes if they're not already)
-  */
-Tree.prototype.addChild = function(child){
-  if (!child || !(child instanceof Tree)){
+ * add an immediate child
+ * (wrap values in Tree nodes if they're not already)
+ */
+Tree.prototype.addChild = function(child) {
+  if (!child || !(child instanceof Tree)) {
     child = new Tree(child);
   }
 
-  if(!this.isDescendant(child)){
+  if (!this.isDescendant(child)) {
     this.children.push(child);
-  }else {
+  } else {
     throw new Error("That child is already a child of this tree");
   }
   // return the new child node for convenience
@@ -72,16 +72,16 @@ Tree.prototype.addChild = function(child){
 };
 
 /**
-  * check to see if the provided tree is already a child of this
-  * tree __or any of its sub trees__
-  */
-Tree.prototype.isDescendant = function(child){
-  if(this.children.indexOf(child) !== -1){
+ * check to see if the provided tree is already a child of this
+ * tree __or any of its sub trees__
+ */
+Tree.prototype.isDescendant = function(child) {
+  if (this.children.indexOf(child) !== -1) {
     // `child` is an immediate child of this tree
     return true;
-  }else{
-    for(var i = 0; i < this.children.length; i++){
-      if(this.children[i].isDescendant(child)){
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].isDescendant(child)) {
         // `child` is descendant of this tree
         return true;
       }
@@ -91,14 +91,14 @@ Tree.prototype.isDescendant = function(child){
 };
 
 /**
-  * remove an immediate child
-  */
-Tree.prototype.removeChild = function(child){
+ * remove an immediate child
+ */
+Tree.prototype.removeChild = function(child) {
   var index = this.children.indexOf(child);
-  if(index !== -1){
+  if (index !== -1) {
     // remove the child
-    this.children.splice(index,1);
-  }else{
+    this.children.splice(index, 1);
+  } else {
     throw new Error("That node is not an immediate child of this tree");
   }
 };
